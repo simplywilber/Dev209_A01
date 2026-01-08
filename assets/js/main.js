bc533ef2228b4f23b9a22881dec5605955314d7f
simplywilber <simplywilber@gmail.com>
2026-01-07 17:35:11 -0800

    Replace all static dates with a dynamic version

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,20 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// Create dynamic date
+const today = new Date();
+const formattedDate = today.toLocaleDateString('default', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  document.getElementById("current-date").innerHTML = formattedDate;
+
+
+
+
+
 (function($) {
 
 	var	$window = $(window),
